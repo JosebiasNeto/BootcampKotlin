b2185da4d413b8f35387d17af5bca90cfd5007b6
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-24 10:35:43 -0300

    Add items on list

--- app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.bootcampkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Neto",
+                    "081",
+                    "img.png"
+                ),
+                        Contact(
+                        "Neto",
+                "081",
+                "img.png"
+            )
+            )
+        )
     }
 }
