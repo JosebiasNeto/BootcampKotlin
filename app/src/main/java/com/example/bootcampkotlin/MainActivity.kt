25c5aab5e01507deb7c80eb722729444c2b985d9
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-24 12:13:07 -0300

    Implement click on list

--- app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/com/example/bootcampkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bootcampkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
@@ -14,12 +15,12 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickItemContactListener {
 
     private val rvList: RecyclerView by lazy {
         findViewById(R.id.rv_list)
     }
-    private val adapter = ContactAdapter()
+    private val adapter = ContactAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,4 +86,11 @@ class MainActivity : AppCompatActivity() {
                 else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun clickItemContact(contact: Contact) {
+        val intent = Intent(this,ContactDetail::class.java)
+        startActivity(intent)
+    }
+
+
 }
